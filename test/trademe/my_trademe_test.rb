7b3cbd16a7787cb78b4ca9430e7033f2854b37e6
Oliver Clarke <ollie@clarketus.net>
2011-04-30 22:26:02 -0700

    added remaining tests minus actual mock json

--- test/trademe/my_trademe_test.rb
+++ test/trademe/my_trademe_test.rb
@@ -14,20 +14,54 @@ class MyTrademeTest < Test::Unit::TestCase
       :delivery_addresses => nil
     }
     
-    methods.each do |method, criteria|
-      context "#{method} make request" do
-        should "raise as not authenticated" do
-          assert_raises Trademe::MustBeAuthenticated do
+    context "when not authenticated" do
+      
+      setup do
+        assert !@gateway.authorized?
+      end
+      
+      methods.each do |method, criteria|
+        context "make #{method} request" do
+          should "raise as not authenticated" do
+            assert_raises Trademe::MustBeAuthenticated do
+              @response = if criteria
+                @gateway.send(method, criteria)
+              else
+                @gateway.send(method)
+              end
+            end
+          end
+        end
+      end
+    end
+    
+    context "when authenticated" do
+      setup do
+        @gateway.expects(:authorized?).at_least_once.returns(true)
+        assert @gateway.authorized?
+      end
+      
+      methods.each do |method, criteria|
+        context "make #{method} request" do
+          setup do
+            success_response = Net::HTTPSuccess.new('foo', 200, 'Success')
+            success_response.stubs(:body).returns("[]") # TODO: need to use the API to grab some test json and throw it in a mock.
+            @gateway.consumer.expects(:request).at_least_once.returns(success_response)
+            
             @response = if criteria
               @gateway.send(method, criteria)
             else
               @gateway.send(method)
             end
           end
+          
+          should "have a response" do
+            assert @response == []
+          end
         end
-      end   
+      end
     end
-    
+        
   end
   
 end
